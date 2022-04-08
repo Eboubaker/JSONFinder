266c49b7c6231bccf11ec15fcaf0dd7e3a3aecf4
Eboubaker Bekkouche <eboubakkar@gmail.com>
2022-04-08 12:38:32 +0100

    added filtering tests.

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -104,8 +104,33 @@ final class UnitTest extends TestCase
         }
         $this->assertEquals('20fb1ea35afd63259651a22d3c836b99', md5($str));
         foreach ($found as $key => $item) {
+            /** @noinspection PhpArrayAccessCanBeReplacedWithForeachValueInspection */
             $this->assertEquals($item, $found[$key]);
         }
     }
 
+    /**
+     * @coversNothing
+     */
+    public function testCanDoFiltering(): void
+    {
+        $count = fn($types) => (new JSONFinder($types))->findJsonEntries($this->rawHTMLResponse)->count();
+        //@formatter:off
+        $null=6;$bool=29;$num=320;$str=1170;$obj=72;$arr=94;$e_obj=28;$e_arr=65;
+        $a_obj = $e_obj + $obj;
+        $a_arr = $e_arr + $arr;
+        $all = $a_arr + $a_obj + $str + $num + $bool + $null;
+        //@formatter:on
+        $this->assertEquals($null, $count(JSONFinder::T_NULL));
+        $this->assertEquals($bool, $count(JSONFinder::T_BOOL));
+        $this->assertEquals($num, $count(JSONFinder::T_NUMBER));
+        $this->assertEquals($str, $count(JSONFinder::T_STRING));
+        $this->assertEquals($arr, $count(JSONFinder::T_ARRAY));
+        $this->assertEquals($obj, $count(JSONFinder::T_OBJECT));
+        $this->assertEquals($e_arr, $count(JSONFinder::T_EMPTY_ARRAY));
+        $this->assertEquals($e_obj, $count(JSONFinder::T_EMPTY_OBJECT));
+        $this->assertEquals($a_arr, $count(JSONFinder::T_EMPTY_ARRAY | JSONFinder::T_ARRAY));
+        $this->assertEquals($a_obj, $count(JSONFinder::T_EMPTY_OBJECT | JSONFinder::T_OBJECT));
+        $this->assertEquals($all, $count(JSONFinder::T_ALL));
+    }
 }
