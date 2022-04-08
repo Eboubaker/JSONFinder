525457761e467bd6a8cc1811f7496e3e2074d09d
Eboubaker Bekkouche <eboubakkar@gmail.com>
2022-04-08 14:55:41 +0100

    update tests. last bug fixes changed output of tests, now tests work again.

--- tests/UnitTest.php
+++ tests/UnitTest.php
@@ -59,7 +59,7 @@ final class UnitTest extends TestCase
     public function testToReadableStringDidNotChangeOutCome(): void
     {
         $parsed = (new JSONFinder)->findJsonEntries($this->rawHTMLResponse);
-        $this->assertEquals("567ce8bd75a13b9c8277f0c0bf526c94", md5($parsed->toReadableString(2)));
+        $this->assertEquals("f8cb82c5544ed1fb18a1a3c3eb099eaa", md5($parsed->toReadableString(2)));
     }
 
     /**
@@ -67,9 +67,19 @@ final class UnitTest extends TestCase
      */
     public function testCanCountEntries(): void
     {
-        $count = (new JSONFinder(JSONFinder::T_ALL))->findJsonEntries($this->validJSONString)->countContainedEntries();
-        $this->assertEquals(39, $count);
+        $count = (new JSONFinder(JSONFinder::T_ALL_JSON))->findJsonEntries($this->rawHTMLResponse)->count();
+        $this->assertEquals(418, $count);
     }
+
+    /**
+     * @coversNothing
+     */
+    public function testCanCountEntriesWithJS(): void
+    {
+        $count = (new JSONFinder(JSONFinder::T_ALL_JSON | JSONFinder::T_JS))->findJsonEntries($this->rawHTMLResponse)->count();
+        $this->assertEquals(224, $count);
+    }
+
 
     /**
      * @coversNothing
@@ -86,10 +96,19 @@ final class UnitTest extends TestCase
     /**
      * @coversNothing
      */
-    public function testCanFindJsonInHtmlResponse(): void
+    public function testCanCountAllContainedEntries(): void
     {
         $parsed = (new JSONFinder)->findJsonEntries($this->rawHTMLResponse);
-        $this->assertEquals(1176, $parsed->countContainedEntries());
+        $this->assertEquals(1732, $parsed->countContainedEntries());
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testCanCountAllContainedEntriesWithJS(): void
+    {
+        $parsed = (new JSONFinder(JSONFinder::T_ARRAY | JSONFinder::T_OBJECT | JSONFinder::T_JS))->findJsonEntries($this->rawHTMLResponse);
+        $this->assertEquals(1857, $parsed->countContainedEntries());
     }
 
     /**
@@ -102,7 +121,7 @@ final class UnitTest extends TestCase
         foreach ($found->values() as $key => $item) {
             $str .= ":$key::$item:";
         }
-        $this->assertEquals('20fb1ea35afd63259651a22d3c836b99', md5($str));
+        $this->assertEquals('3eabae12402f4d558476c0f29a57abb2', md5($str));
         foreach ($found as $key => $item) {
             /** @noinspection PhpArrayAccessCanBeReplacedWithForeachValueInspection */
             $this->assertEquals($item, $found[$key]);
@@ -116,11 +135,11 @@ final class UnitTest extends TestCase
     {
         $count = fn($types) => (new JSONFinder($types))->findJsonEntries($this->rawHTMLResponse)->count();
         //@formatter:off
-        $null=6;$bool=29;$num=320;$str=1170;$obj=72;$arr=94;$e_obj=28;$e_arr=65;
+        $null=7;$bool=22;$num=77;$str=217;$obj=6;$arr=61;$e_obj=16;$e_arr=12;
+        //@formatter:on
         $a_obj = $e_obj + $obj;
         $a_arr = $e_arr + $arr;
         $all = $a_arr + $a_obj + $str + $num + $bool + $null;
-        //@formatter:on
         $this->assertEquals($null, $count(JSONFinder::T_NULL));
         $this->assertEquals($bool, $count(JSONFinder::T_BOOL));
         $this->assertEquals($num, $count(JSONFinder::T_NUMBER));
@@ -131,6 +150,32 @@ final class UnitTest extends TestCase
         $this->assertEquals($e_obj, $count(JSONFinder::T_EMPTY_OBJECT));
         $this->assertEquals($a_arr, $count(JSONFinder::T_EMPTY_ARRAY | JSONFinder::T_ARRAY));
         $this->assertEquals($a_obj, $count(JSONFinder::T_EMPTY_OBJECT | JSONFinder::T_OBJECT));
-        $this->assertEquals($all, $count(JSONFinder::T_ALL));
+        $this->assertEquals($all, $count(JSONFinder::T_ALL_JSON));
+    }
+
+    /**
+     * @coversNothing
+     * @testdox can do filtering with javascript flag on
+     */
+    public function testCanDoFilteringWithJSFlag(): void
+    {
+        $count = fn($types) => (new JSONFinder($types))->findJsonEntries($this->rawHTMLResponse)->count();
+        //@formatter:off
+        $null=1;$bool=2;$num=19;$str=158;$obj=8;$arr=19;$e_obj=16;$e_arr=1;
+        //@formatter:on
+        $a_obj = $e_obj + $obj;
+        $a_arr = $e_arr + $arr;
+        $all = $a_arr + $a_obj + $str + $num + $bool + $null;
+        $this->assertEquals($null, $count(JSONFinder::T_NULL | JSONFinder::T_JS));
+        $this->assertEquals($bool, $count(JSONFinder::T_BOOL | JSONFinder::T_JS));
+        $this->assertEquals($num, $count(JSONFinder::T_NUMBER | JSONFinder::T_JS));
+        $this->assertEquals($str, $count(JSONFinder::T_STRING | JSONFinder::T_JS));
+        $this->assertEquals($arr, $count(JSONFinder::T_ARRAY | JSONFinder::T_JS));
+        $this->assertEquals($obj, $count(JSONFinder::T_OBJECT | JSONFinder::T_JS));
+        $this->assertEquals($e_arr, $count(JSONFinder::T_EMPTY_ARRAY | JSONFinder::T_JS));
+        $this->assertEquals($e_obj, $count(JSONFinder::T_EMPTY_OBJECT | JSONFinder::T_JS));
+        $this->assertEquals($a_arr, $count(JSONFinder::T_EMPTY_ARRAY | JSONFinder::T_ARRAY | JSONFinder::T_JS));
+        $this->assertEquals($a_obj, $count(JSONFinder::T_EMPTY_OBJECT | JSONFinder::T_OBJECT | JSONFinder::T_JS));
+        $this->assertEquals($all, $count(JSONFinder::T_ALL_JSON | JSONFinder::T_JS));
     }
 }
