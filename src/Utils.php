fd1d0c1494f627f3f7c08f80f142c8822cf0f13c
Eboubaker Bekkouche <eboubakkar@gmail.com>
2022-04-15 15:20:46 +0100

    remove unused function Utils::is_associative()

--- src/Utils.php
+++ src/Utils.php
@@ -8,19 +8,6 @@ namespace Eboubaker\JSON;
  */
 final class Utils
 {
-    /**
-     * returns true if $enumerable contains a string key. and false if $enumerable keys are all integers
-     */
-    static function is_associative($enumerable): bool
-    {
-        foreach ($enumerable as $key => $v) {
-            if (!is_int($key)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     static function typeof($value): string
     {
         if (is_object($value)) {
